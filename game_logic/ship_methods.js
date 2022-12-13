ef5070d4741e2b1d7e726262bd2af4f1e82dc073
Carmen Chow <carmendevelops@gmail.com>
2022-12-13 12:28:11 -0500

    make tests pass

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,8 +1,22 @@
-function checkForShip(){
+function checkForShip(player, coordinates){
+  var shipPresent, ship;
 
+  // Loop through ships in the array and check each of their coordinates against the one given in the function
+  for (var i=0; i < player.ships.length; i++) {
+    // Save current ship in the loop
+    ship = player.ships[i];
+    // Filter ships location array by comparing values to given coordinates
+    shipPresent = ship.locations.filter(function(actualCoordinate){
+      // return an array with only coordainates that match
+      return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1]);
+    })[0]; // The first member of an empty array is undefined
+
+    if (!shipPresent){
+      return false;
+    }
+  
+  }
 }
-
-
 
 // Export using module
 module.exports.checkForShip = checkForShip;
--- test/ship_test.js
+++ test/ship_test.js
@@ -8,8 +8,19 @@ describe('checkForShip', function(){
   // import 'checkForShip so all our tests in the suite can access it. Code will break because we haven't made this file yet:
   var checkForShip = require('../game_logic/ship_methods.js').checkForShip;
 
-
   it('should correctly report no ship at a given players coordinate', function(){
+
+    // Represent player as object
+    player = {
+      ships: [
+        {
+          locations: [[0, 0]]
+        }
+      ]
+    };
+
+    // There should be no ships at this given coordinate
+    expect(checkForShip(player, [9,9])).to.be.false;
     
   });
 });
